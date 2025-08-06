c6aefd5befab2be5e6be1938481d401cbe1c80cf
HoNgocAn <anhongoc1306@gmail.com>
2025-08-06 10:28:34 +0700

    up

--- chat-app-be/src/models/db.ts
+++ chat-app-be/src/models/db.ts
@@ -8,7 +8,10 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false // Render requires this
+    }
 });
 
 // Kết nối thử để log kết quả
